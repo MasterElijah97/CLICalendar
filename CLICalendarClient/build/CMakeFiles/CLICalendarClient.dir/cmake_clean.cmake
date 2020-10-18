29f9a5c12ba14ee77d1b26c375e74d5b644834ee
MasterElijah97 <morozov97@mail.ru>
2020-10-18 20:41:25 +0300

    Begin writing CMakes

---
+++ CLICalendarClient/build/CMakeFiles/CLICalendarClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLICalendarClient"
+  "CLICalendarClient.pdb"
+  "CMakeFiles/CLICalendarClient.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLICalendarClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
