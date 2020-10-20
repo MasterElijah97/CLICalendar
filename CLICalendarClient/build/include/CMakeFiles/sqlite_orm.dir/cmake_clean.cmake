1aea864c1480ef00b6b757d98ef69c1882af719b
MasterElijah97 <morozov97@mail.ru>
2020-10-20 20:28:42 +0300

    Fixed gitignore

--- CLICalendarClient/build/include/CMakeFiles/sqlite_orm.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsqlite_orm.a"
-  "libsqlite_orm.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sqlite_orm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
